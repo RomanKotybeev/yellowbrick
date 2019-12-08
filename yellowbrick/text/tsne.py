c9c685c40e45783977a0dd8c671789dd4d497634
RomanKotyubeev <45431768+RomanKotybeev@users.noreply.github.com>
2019-12-08 14:07:40 +0700

    added annotations to have the text is placed at xy

--- yellowbrick/text/tsne.py
+++ yellowbrick/text/tsne.py
@@ -382,6 +382,8 @@ class TSNEVisualizer(TextVisualizer):
             self.ax.scatter(
                 points["x"], points["y"], c=colors[label], alpha=self.alpha, label=label
             )
+            for label, x, y in zip(labels,points["x"],points["y"]):
+                ax.annotate(label,(x,y))
 
         return self.ax
 
